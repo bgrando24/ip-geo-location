d544e222ad34ab7d244033d8ef2632748aa7b812
bgrando24 <bgrando24@gmail.com>
2024-04-27 20:45:18 +1000

    Initial commit

---
+++ src/main/java/com/example/ipgeolocation/model/IpResponse.java
@@ -0,0 +1,2 @@
+package com.example.ipgeolocation.model;public class IpResponse {
+}
---
+++ src/test/java/com/example/ipgeolocation/IpGeoLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ipgeolocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpGeoLocationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
