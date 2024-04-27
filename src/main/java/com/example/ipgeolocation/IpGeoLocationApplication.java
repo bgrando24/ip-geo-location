c5fe5fd77a0983eb805adf8757ee5a3192399987
bgrando24 <bgrando24@gmail.com>
2024-04-27 20:45:37 +1000

    init

--- src/main/java/com/example/ipgeolocation/IpGeoLocationApplication.java
+++ src/main/java/com/example/ipgeolocation/IpGeoLocationApplication.java
@@ -2,11 +2,14 @@ package com.example.ipgeolocation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class IpGeoLocationApplication {
 
     public static void main(String[] args) {
+//        run app
         SpringApplication.run(IpGeoLocationApplication.class, args);
     }
 
--- src/main/java/com/example/ipgeolocation/model/IpResponse.java
+++ src/main/java/com/example/ipgeolocation/model/IpResponse.java
@@ -1,2 +1,63 @@
-package com.example.ipgeolocation.model;public class IpResponse {
+package com.example.ipgeolocation.model;
+
+/**
+ * A model of the IP Geolocation API response
+ * Reference: https://ip-api.com/docs/api:json
+ */
+public class IpResponse {
+//    instance variables
+    public String success;
+    public String country;
+    public String countryCode;
+    public String region;
+    public String regionName;
+    public String city;
+    public String zip;
+    public double lat;
+    public double lon;
+    public String timezone;
+    public String isp;
+
+//    constructor
+    public IpResponse(
+            String success,
+            String country,
+            String countryCode,
+            String region,
+            String regionName,
+            String city,
+            String zip,
+            double lat,
+            double lon,
+            String timezone,
+            String isp
+    ) {
+        this.success = success;
+        this.country = country;
+        this.countryCode = countryCode;
+        this.region = region;
+        this.regionName = regionName;
+        this.city = city;
+        this.zip = zip;
+        this.lat = lat;
+        this.lon = lon;
+        this.timezone = timezone;
+        this.isp = isp;
+    }
 }
+
+
+//status	"success"
+//country	"Australia"
+//countryCode	"AU"
+//region	"VIC"
+//regionName	"Victoria"
+//city	"Melbourne"
+//zip	"3141"
+//lat	-37.834
+//lon	144.9832
+//timezone	"Australia/Melbourne"
+//isp	"Aussie Broadband"
+//org	""
+//as	"AS4764 Aussie Broadband"
+//query	"180.150.32.208"
